d92a3230dafa4dc5b72a3dd97d3d27f13aab5ec6
artbooms <info@artbooms.com>
2025-08-05 01:54:04 +0200

    30Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ HEADERS = {
 }
 
 REPLACE_MAP = {
-    '\u2019': "'",
-    '\u201c': '"',
-    '\u201d': '"',
-    '\u00a0': ' ',
-    '\u2014': '-',
-    '\u2013': '-',
+    u'\u2019': "'",
+    u'\u201c': '"',
+    u'\u201d': '"',
+    u'\u00a0': ' ',
+    u'\u2014': '-',
+    u'\u2013': '-',
 }
 
 def clean_text(text):
@@ -38,7 +38,12 @@ def get_month_links():
 def get_article_links(month_url):
     res = requests.get(month_url, headers=HEADERS)
     soup = BeautifulSoup(res.text, "html.parser")
-    return [BASE_URL + a.get("href") for a in soup.select("article a") if a.get("href")]
+    article_links = []
+    for a in soup.select("article a"):
+        href = a.get("href")
+        if href:
+            article_links.append(BASE_URL + href)
+    return article_links
 
 def parse_article(url):
     res = requests.get(url, headers=HEADERS)
@@ -76,12 +81,33 @@ def rss():
                 article = parse_article(link)
                 item = (
                     "<item>"
-                    f"<title><![CDATA[{article['title']}]]></title>"
-                    f"<link>{article['link']}</link>"
-                    f"<description><![CDATA[{article['description']}]]></description>"
-                    f"<pubDate>{article['pubDate']}</pubDate>"
-                    f"<category>{article['category']}</category>"
-                    f"<enclosure url=\"{article['image']}\" type=\"image/jpeg\" />"
+                    "<title><![CDATA[%s]]></title>" % article['title'] +
+                    "<link>%s</link>" % article['link'] +
+                    "<description><![CDATA[%s]]></description>" % article['description'] +
+                    "<pubDate>%s</pubDate>" % article['pubDate'] +
+                    "<category>%s</category>" % article['category'] +
+                    '<enclosure url="%s" type="image/jpeg" />' % article['image'] +
                     "</item>"
                 )
                 items.append(item)
+            except Exception:
+                # Ignora errori per evitare crash
+                continue
+
+    rss_feed = (
+        '<?xml version="1.0" encoding="UTF-8"?>'
+        '<rss version="2.0">'
+        '<channel>'
+        '<title>Artbooms</title>'
+        '<link>%s</link>' % BASE_URL +
+        '<description>Artbooms RSS Feed</description>'
+        '<language>it-it</language>'
+        '%s' % ''.join(items) +
+        '</channel>'
+        '</rss>'
+    )
+
+    return Response(rss_feed, mimetype='application/rss+xml')
+
+if __name__ == "__main__":
+    app.run(debug=True)
