4bda60618807aff160503b8102a87365c906e893
artbooms <info@artbooms.com>
2025-08-07 02:11:39 +0200

    volta buonaUpdate article_parser.py

--- article_parser.py
+++ article_parser.py
@@ -23,17 +23,41 @@ def parse_article(url):
             description = str(content)
 
         # Immagine (opzionale)
-        image_tag = soup.find('img')
-        image = image_tag['src'] if image_tag and image_tag.has_attr('src') else None
+        image_tag = soup.find('meta', property='og:image')
+        if image_tag and image_tag.has_attr('content'):
+            image = image_tag['content']
+        else:
+            image = None
 
-        # Data fittizia (nel formato RSS)
-        pub_date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        # Autore (opzionale)
+        author_tag = soup.find('meta', itemprop='author')
+        author = author_tag['content'] if author_tag and author_tag.has_attr('content') else None
+
+        # DatePublished e DateModified (opzionali)
+        date_modified_tag = soup.find('meta', itemprop='dateModified')
+        date_modified = date_modified_tag['content'] if date_modified_tag and date_modified_tag.has_attr('content') else None
+
+        date_published_tag = soup.find('meta', itemprop='datePublished')
+        date_published = date_published_tag['content'] if date_published_tag and date_published_tag.has_attr('content') else None
+
+        # Fallback pubDate in formato RSS (se non trovato da meta)
+        pub_date = None
+        if date_published:
+            try:
+                # converte ISO 8601 in formato RSS RFC 2822
+                dt = datetime.datetime.fromisoformat(date_published.replace('Z', '+00:00'))
+                pub_date = dt.strftime('%a, %d %b %Y %H:%M:%S GMT')
+            except Exception:
+                pub_date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        else:
+            pub_date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
 
         return {
             "description": description,
             "image": image,
-            "author": None,
+            "author": author,
             "pubDate": pub_date,
+            "dateModified": date_modified,
         }
 
     except Exception as e:
@@ -42,4 +66,5 @@ def parse_article(url):
             "image": None,
             "author": None,
             "pubDate": datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'),
+            "dateModified": None,
         }
