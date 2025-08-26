61a9b414cbe83afa108144b7f458068c2c9b18df
artbooms <info@artbooms.com>
2025-08-26 04:25:08 +0200

    Update article_parser.py

--- article_parser.py
+++ article_parser.py
@@ -11,4 +11,28 @@ MONTHS_IT = {
 
 DATE_RX = re.compile(
     r"\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t(?:ember)?)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?|"
-    + "|".join(MONTHS_IT.keys()) + r")[\s\-]+\d{1,2},?[\s\-]+\d{4}\b",_
+    + "|".join(MONTHS_IT.keys()) + r")[\s\-]+\d{1,2},?[\s\-]+\d{4}\b",
+    re.I
+)
+
+def normalize_date(txt: str):
+    if not txt:
+        return None
+    low = txt.strip()
+    for it, en in MONTHS_IT.items():
+        low = re.sub(rf"\b{re.escape(it)}\b", en, low, flags=re.I)
+    try:
+        return dateparser.parse(low, dayfirst=False)
+    except Exception:
+        return None
+
+def extract_article_links(archive_html: str, base_url: str):
+    soup = BeautifulSoup(archive_html, "lxml")
+    links = []
+    for a in soup.find_all("a", href=True):
+        href = a["href"].strip()
+        if any(domain in href for domain in ["facebook.com","instagram.com","x.com","twitter.com","pinterest.com","feeds.feedburner.com"]):
+            continue
+        if "/blog/" in href:
+            full = urljoin(base_url, href)
+            if full
