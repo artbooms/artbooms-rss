021a47bd86ef0370fdfa0df338e69130932b85f5
artbooms <info@artbooms.com>
2025-08-26 04:02:36 +0200

    Update rss_generator.py

--- rss_generator.py
+++ rss_generator.py
@@ -1,81 +1,58 @@
-from xml.etree import ElementTree as ET
-from xml.etree.ElementTree import Element, SubElement
+import hashlib
 from datetime import datetime, timezone
 from email.utils import format_datetime
-import hashlib
-
-NS = {
-    'atom': 'http://www.w3.org/2005/Atom',
-    'dc': 'http://purl.org/dc/elements/1.1/',
-    'dcterms': 'http://purl.org/dc/terms/',
-    'media': 'http://search.yahoo.com/mrss/'
-}
-for p, uri in NS.items():
-    ET.register_namespace(p, uri)
-
+from feedgen.feed import FeedGenerator
 
 def _as_dt(s):
     if isinstance(s, datetime):
         return s
-    if not s:
-        return None
     try:
-        s = s.replace('Z', '+00:00')
         return datetime.fromisoformat(s)
     except Exception:
         return None
 
+def build_rss(items: list[dict], meta: dict):
+    fg = FeedGenerator()
+    fg.title(meta.get('title'))
+    fg.description(meta.get('description'))
+    if meta.get('self_url'):
+        fg.link(href=meta['self_url'], rel='self')
+    fg.link(href='https://www.artbooms.com', rel='alternate')
+    fg.language(meta.get('language', 'it-IT'))
 
-def build_rss(items, meta):
-    rss = Element('rss', attrib={'version': '2.0', 'xmlns:atom': NS['atom'], 'xmlns:dc': NS['dc'], 'xmlns:dcterms': NS['dcterms'], 'xmlns:media': NS['media']})
-    ch = SubElement(rss, 'channel')
-
-    SubElement(ch, 'title').text = meta.get('title')
-    SubElement(ch, 'link').text = 'https://www.artbooms.com'
-    SubElement(ch, 'description').text = meta.get('description')
-
-    self_url = meta.get('self_url')
-    if self_url:
-        SubElement(ch, f"{{{NS['atom']}}}link", attrib={'href': self_url, 'rel': 'self', 'type': 'application/rss+xml'})
-
-    last_mod = None
+    last_modified = None
 
     for it in items:
-        item = SubElement(ch, 'item')
-        link = it.get('url')
-        SubElement(item, 'title').text = it.get('title') or link
-        SubElement(item, 'link').text = link
-        SubElement(item, 'guid', attrib={'isPermaLink': 'true'}).text = link
-
+        fe = fg.add_entry()
+        fe.id(it.get('url'), permalink=True)
+        fe.link(href=it.get('url'))
+        fe.title(it.get('title') or it.get('url'))
         if it.get('description'):
-            SubElement(item, 'description').text = it.get('description')
+            fe.description(it['description'])
         if it.get('author'):
-            SubElement(item, f"{{{NS['dc']}}}creator").text = it.get('author')
+            fe.author({'name': it['author']})
 
         pub = _as_dt(it.get('published'))
         mod = _as_dt(it.get('modified')) or pub
         if pub:
-            SubElement(item, 'pubDate').text = format_datetime(pub)
+            fe.pubDate(format_datetime(pub))
         if mod:
-            SubElement(item, f"{{{NS['dcterms']}}}modified").text = format_datetime(mod)
-            if (last_mod is None) or (mod > last_mod):
-                last_mod = mod
+            fe.dc({'creator': it.get('author')})
+            fe.extensions()['content'] = {}
+            fe._FeedEntry__setitem('dcterms:modified', format_datetime(mod))
+            if (last_modified is None) or (mod > last_modified):
+                last_modified = mod
 
-        # immagine come enclosure e media:content
-        img = it.get('image')
-        if img:
-            SubElement(item, 'enclosure', attrib={'url': img, 'type': 'image/jpeg'})
-            SubElement(item, f"{{{NS['media']}}}content", attrib={'url': img, 'medium': 'image'})
+    build_time = meta.get('build_time') or datetime.utcnow().replace(tzinfo=timezone.utc)
+    fg.lastBuildDate(format_datetime(last_modified or build_time))
 
-    # lastBuildDate
-    build = last_mod or datetime.utcnow().replace(tzinfo=timezone.utc)
-    SubElement(ch, 'lastBuildDate').text = format_datetime(build)
+    rss_bytes = fg.rss_str(pretty=True)
+    etag = hashlib.sha256(rss_bytes).hexdigest()
 
-    xml_bytes = ET.tostring(rss, encoding='utf-8')
-    etag = hashlib.sha256(xml_bytes).hexdigest()
     headers = {
         'ETag': f'W/"{etag}"',
-        'Last-Modified': format_datetime(build),
-        'Cache-Control': 'public, max-age=300'
+        'Last-Modified': format_datetime(last_modified or build_time),
+        'Cache-Control': 'max-age=300',
     }
-    return xml_bytes, headers
+
+    return rss_bytes, headers
