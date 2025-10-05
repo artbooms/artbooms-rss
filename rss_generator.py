f0aa01d87ca02816ed85953e9ceab177fe767d35
artbooms <info@artbooms.com>
2025-10-05 23:31:23 +0200

    Update rss_generator.py

--- rss_generator.py
+++ rss_generator.py
@@ -2,9 +2,13 @@ import hashlib
 from datetime import datetime, timezone
 from email.utils import format_datetime
 import logging
+import xml.etree.ElementTree as ET
 
 logger = logging.getLogger("rss_generator")
 
+# ---------------------------------------------------------
+# Utility per conversione date
+# ---------------------------------------------------------
 def _as_dt(s):
     if not s:
         return None
@@ -19,121 +23,101 @@ def _as_dt(s):
     except Exception:
         return None
 
-# try feedgen first, fallback to ElementTree if not installed
+
+# ---------------------------------------------------------
+# Costruzione RSS (fallback XML, no dipendenze esterne)
+# ---------------------------------------------------------
 def build_rss(items: list, meta: dict):
-    try:
-        from feedgen.feed import FeedGenerator
-        return _build_with_feedgen(items, meta)
-    except Exception as e:
-        logger.warning("feedgen not available or failed (%s), using fallback XML builder", e)
-        return _build_fallback(items, meta)
-
-def _build_with_feedgen(items, meta):
-    from feedgen.feed import FeedGenerator
-    fg = FeedGenerator()
-    fg.id(meta.get('self_url') or "")
-    fg.title(meta.get('title') or "ARTBOOMS")
-    fg.description(meta.get('description') or "")
-    fg.link(href=meta.get('self_url') or "", rel='self')
-    fg.language(meta.get('language') or "it-IT")
-
-    last_modified = None
-    for it in items:
-        fe = fg.add_entry()
-        fe.id(it.get('url'))
-        fe.title(it.get('title') or "")
-        fe.link(href=it.get('url'))
-        if it.get('description'):
-            fe.description(it.get('description'))
-        author = it.get('author')
-        if author:
-            try:
-                fe.author({'name': author})
-            except Exception:
-                fe._FeedEntry__setitem('dc:creator', author)
-        pub = _as_dt(it.get('published'))
-        if pub:
-            fe.pubDate(format_datetime(pub))
-        mod = _as_dt(it.get('modified'))
-        if mod:
-            try:
-                fe._FeedEntry__setitem('dcterms:modified', format_datetime(mod))
-            except Exception:
-                pass
-            if (last_modified is None) or (mod > last_modified):
-                last_modified = mod
-        image_url = it.get('image')
-        if image_url:
-            try:
-                fe.enclosure(image_url, 0, 'image/jpeg')
-            except Exception:
-                pass
-            try:
-                fe._FeedEntry__setitem('media:thumbnail', {'url': image_url})
-            except Exception:
-                pass
-
-    build_time = meta.get('build_time') or datetime.utcnow().replace(tzinfo=timezone.utc)
-    fg.lastBuildDate(format_datetime(last_modified or build_time))
-    rss_bytes = fg.rss_str(pretty=True)
-    etag = hashlib.sha256(rss_bytes).hexdigest()
-    headers = {
-        'ETag': f'W/"{etag}"',
-        'Last-Modified': format_datetime(last_modified or build_time),
-        'Cache-Control': 'max-age=300',
-    }
-    return rss_bytes, headers
-
-def _build_fallback(items, meta):
-    # minimale ma compatibile XML RSS 2.0 con namespace media e dc
-    import xml.etree.ElementTree as ET
-    ET.register_namespace("media", "http://search.yahoo.com/mrss/")
+    """
+    Crea un feed RSS 2.0 con namespace DC, media e atom compatibile con Google News.
+    """
+    # Namespace
     ET.register_namespace("dc", "http://purl.org/dc/elements/1.1/")
+    ET.register_namespace("media", "http://search.yahoo.com/mrss/")
+    ET.register_namespace("atom", "http://www.w3.org/2005/Atom")
+    ET.register_namespace("dcterms", "http://purl.org/dc/terms/")
+
     rss = ET.Element("rss", version="2.0")
     channel = ET.SubElement(rss, "channel")
-    ET.SubElement(channel, "title").text = meta.get('title') or "ARTBOOMS"
-    ET.SubElement(channel, "link").text = meta.get('self_url') or ""
-    ET.SubElement(channel, "description").text = meta.get('description') or ""
-    if meta.get('language'):
-        ET.SubElement(channel, "language").text = meta.get('language')
+
+    # -----------------------------
+    # Meta canale
+    # -----------------------------
+    title = meta.get("title", "ARTBOOMS - Archivio completo")
+    self_url = meta.get("self_url", "https://artbooms-rss.onrender.com/rss")
+    desc = meta.get("description", "Tutti gli articoli di Artbooms con aggiornamenti automatici")
+    lang = meta.get("language", "it-IT")
+
+    ET.SubElement(channel, "title").text = title
+    ET.SubElement(channel, "link").text = "https://www.artbooms.com"
+    ET.SubElement(channel, "description").text = desc
+    ET.SubElement(channel, "language").text = lang
+
+    # Atom self-link
+    atom_link = ET.SubElement(channel, "{http://www.w3.org/2005/Atom}link")
+    atom_link.set("href", self_url)
+    atom_link.set("rel", "self")
+    atom_link.set("type", "application/rss+xml")
+
     last_modified = None
 
+    # -----------------------------
+    # Articoli
+    # -----------------------------
     for it in items:
         item = ET.SubElement(channel, "item")
-        ET.SubElement(item, "title").text = it.get('title') or ""
-        ET.SubElement(item, "link").text = it.get('url') or ""
-        ET.SubElement(item, "guid").text = it.get('url') or ""
-        if it.get('description'):
-            desc = ET.SubElement(item, "description")
-            desc.text = it.get('description')
-        author = it.get('author')
+        title = it.get("title") or ""
+        url = it.get("url") or ""
+        desc = it.get("description") or ""
+        author = it.get("author") or None
+        pub = _as_dt(it.get("published"))
+        mod = _as_dt(it.get("modified"))
+        image = it.get("image")
+
+        ET.SubElement(item, "title").text = title
+        ET.SubElement(item, "link").text = url
+
+        guid = ET.SubElement(item, "guid")
+        guid.text = url
+        guid.set("isPermaLink", "true")
+
+        if desc:
+            ET.SubElement(item, "description").text = desc
+
         if author:
             dc_creator = ET.SubElement(item, "{http://purl.org/dc/elements/1.1/}creator")
             dc_creator.text = author
-        pub = _as_dt(it.get('published'))
+
         if pub:
+            # formato RFC 822 richiesto da RSS standard
             ET.SubElement(item, "pubDate").text = format_datetime(pub)
-        mod = _as_dt(it.get('modified'))
+
         if mod:
-            last_modified = mod if (last_modified is None or mod > last_modified) else last_modified
-            # dcterms:modified not standard in ET convenience, aggiungiamo come elemento semplice
-            modtag = ET.SubElement(item, "{http://purl.org/dc/terms/}modified")
-            modtag.text = format_datetime(mod)
-        image = it.get('image')
+            # formato W3CDTF (Google News preferisce ISO 8601)
+            ET.SubElement(item, "{http://purl.org/dc/terms/}modified").text = mod.astimezone(timezone.utc).isoformat()
+            if (last_modified is None) or (mod > last_modified):
+                last_modified = mod
+
         if image:
-            # media:thumbnail
             thumb = ET.SubElement(item, "{http://search.yahoo.com/mrss/}thumbnail")
             thumb.set("url", image)
 
-    build_time = meta.get('build_time') or datetime.utcnow().replace(tzinfo=timezone.utc)
-    # lastBuildDate come elemento channel
+    # -----------------------------
+    # Ultima modifica globale
+    # -----------------------------
+    build_time = meta.get("build_time") or datetime.utcnow().replace(tzinfo=timezone.utc)
     ET.SubElement(channel, "lastBuildDate").text = format_datetime(last_modified or build_time)
 
+    # -----------------------------
+    # Conversione in bytes + header
+    # -----------------------------
     xml_bytes = ET.tostring(rss, encoding="utf-8", xml_declaration=True)
     etag = hashlib.sha256(xml_bytes).hexdigest()
     headers = {
-        'ETag': f'W/"{etag}"',
-        'Last-Modified': format_datetime(last_modified or build_time),
-        'Cache-Control': 'max-age=300',
+        "ETag": f'W/"{etag}"',
+        "Last-Modified": format_datetime(last_modified or build_time),
+        "Cache-Control": "max-age=300",
+        "Content-Type": "application/rss+xml; charset=utf-8",
     }
+
     return xml_bytes, headers
